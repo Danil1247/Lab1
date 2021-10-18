0df342dc12eabfc435bd512c38f5b9623736a7df
Danil1247 <90216234+Danil1247@users.noreply.github.com>
2021-10-18 19:29:10 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+#include <malloc.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	double min, max, drob, d, sum1 = 0;
+	int k, g, n;
+	printf("¬ведите количество: ");
+	scanf_s("%d", &n);
+	if (n <= 0)
+	{
+		printf("Error, n <= 0");
+		return 0;
+	}
+	printf("¬ведите минимум: ");
+	scanf_s("%lf", &min);
+	printf("¬ведите максимум: ");
+	scanf_s("%lf", &max);
+	if (min > max)
+	{
+		printf("Error, min > max");
+		return 0;
+	}
+	double* mas = NULL;
+	int* mas1 = NULL;
+	mas = ((double*)malloc(n * sizeof(double) + 1));
+	mas1 = ((int*)malloc(n * sizeof(double) + 1));
+	for (k = 0; k < n; k++)
+	{
+		mas[k] = (((double)rand()) / RAND_MAX * (max - min) + min);
+		drob = modf(mas[k], &d);
+		mas1[k] = (int)ceil(drob * 1000000);
+		sum1 += mas[k];
+	}
+	for (k = 0; k < n; k++)
+	{
+		for (g = 0; g < n; g++)
+		{
+			if (k == mas1[g])
+			{
+				sum1 -= mas[g];
+			}
+		}
+	}
+	printf("—умма равна: %lf\n", sum1);
+	free(mas);
+	free(mas1);
+	return 0;
+}
